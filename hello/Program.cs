08b365d213c289844857101726fdce6ceb95d1b5
Ganesh <ganeshdes6050@gmail.com>
2025-11-06 19:41:59 +0530

    dev changed p

--- hello/Program.cs
+++ hello/Program.cs
@@ -3,3 +3,4 @@ int b = 11;
 System.Console.WriteLine(a+b);
 System.Console.WriteLine("Hello, World!");
 System.Console.WriteLine("This is a simple C# program.");
+System.Console.WriteLine("The sum of a and b is: " + (a + b));
